5ca968955eb2f1aec518c99c86c5734ba9c542a6
ymorii <dimasrizkidwisaputra0@gmail.com>
2025-12-09 23:31:40 +0700

    delete deployment change to render

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Replace environment variables in Nginx config
+# If PORT is not set, default to 8080
+export PORT=${PORT:-8080}
+envsubst '${PORT}' < /etc/nginx/sites-available/default.template > /etc/nginx/sites-available/default
+
 # Cache configuration
 php artisan config:cache
 php artisan route:cache
